861eed52f18483bff4a8a3ecf9d288bfe2e8e7b6
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-27 09:47:53 +0900

    fix: generate Docker image by Dockerfile

--- terraform/modules/keycloak-app/docker/push-ecr-image.sh
+++ terraform/modules/keycloak-app/docker/push-ecr-image.sh
@@ -2,15 +2,16 @@
 set -e
 
 REGION="ap-northeast-1"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "ECRにログイン..."
 aws ecr get-login-password --region "${REGION}" | docker login --username AWS --password-stdin "${ECR_REPOSITORY_URL%%/*}"
 
-echo "イメージを取得..."
-docker pull "quay.io/keycloak/keycloak:${KEYCLOAK_VERSION}"
-
-echo "タグ付け..."
-docker tag "quay.io/keycloak/keycloak:${KEYCLOAK_VERSION}" "${ECR_REPOSITORY_URL}:${KEYCLOAK_VERSION}"
+echo "カスタムイメージをビルド..."
+docker build \
+  -t "${ECR_REPOSITORY_URL}:${KEYCLOAK_VERSION}" \
+  -f "${SCRIPT_DIR}/Dockerfile" \
+  "${SCRIPT_DIR}"
 
 echo "プッシュ..."
 docker push "${ECR_REPOSITORY_URL}:${KEYCLOAK_VERSION}"
